b1fc9501f0f14305c2edbf763ca0c98d5f27c27c
kambhani <63814718+kambhani@users.noreply.github.com>
2024-11-28 22:48:52 -0500

    Add MCTS AI

--- mcts.py
+++ mcts.py
@@ -7,10 +7,26 @@ SEED = 1234
 random.seed(SEED)
 np.random.seed(SEED)
 
-def play_game(game: Game):
+def play_random_moves(game: Game):
     while not game.game_over():
         action = random.choice(game.available_actions())
         game.do_action(action)
+    return game.score()
+
+def play_game(game: Game):
+    while not game.game_over():
+        actions = game.available_actions()
+        best_action = -1
+        best_score = -1
+        for action in actions:
+            game_copy = game.copy()
+            game_copy.do_action(action)
+            scores = [play_random_moves(game_copy.copy()) for _ in range(50)]
+            if np.mean(scores) > best_score:
+                best_score = np.mean(scores)
+                best_action = action
+        game.do_action(best_action)
+
     return game.score(), game.max_tile()
 
 if __name__ == '__main__':
